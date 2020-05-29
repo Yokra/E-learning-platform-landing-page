136aac97c29dc0d8cabfa830488dacef06a50d8f
yokra <yordanka_krasteva@mail.bg>
2020-05-29 17:35:05 +0200

    making the nac fixed

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-/* function myFunction() {
+function myFunction() {
   var x = document.getElementById("menuLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
@@ -15,4 +15,3 @@ function xFunction() {
     y.style.display = "none";
   }
 }
- */
